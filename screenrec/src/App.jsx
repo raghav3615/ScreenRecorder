a88338bc4df071185333defd7bb28c9a3e965c2b
raghav3615 <dadhichraghav896@gmail.com>
2024-10-21 21:56:30 +0530

    changes

--- screenrec/src/App.jsx
+++ screenrec/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ScreenRecorder from './component/ScreenRecorder';
-import Login from './component/Login';
+import Login from './component/login';
 import Signup from './component/Signup'; 
 
 import './App.css';
