8523713b5f4d40a788e775994218e09d63b83a56
raghav3615 <dadhichraghav896@gmail.com>
2024-10-21 21:04:35 +0530

    firebase update

--- screenrec/src/firebase.js
+++ screenrec/src/firebase.js
@@ -5,13 +5,13 @@ import { getAnalytics } from "firebase/analytics"; // Optional: Import analytics
 
 // Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCQk-wxDsqRFnx2Seq0rvzDnmqZCXVYyro",
+  authDomain: "screen-recorder-15d8c.firebaseapp.com",
+  projectId: "screen-recorder-15d8c",
+  storageBucket: "screen-recorder-15d8c.appspot.com",
+  messagingSenderId: "15436247862",
+  appId: "1:15436247862:web:a881d54579ffa96925b997",
+  measurementId: "G-YLSFCB5V8C"
 };
 
 // Initialize Firebase
